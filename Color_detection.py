0fcefa9ffb16b852df622efc46c9c30b1b618242
SuS <vartevanyan.k@mail.ru>
2021-10-27 16:36:43 +0300

    feat: добавили распознавание цвета на видео

--- Color_detection.py
+++ Color_detection.py
@@ -1,13 +1,20 @@
 import cv2
 import numpy as np
 
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+
 def empty(g):
     pass
 
-img: np.ndarray = cv2.imread('robot.jpeg', cv2.IMREAD_COLOR)
-shrink_koeff = 2
-img_h,img_w, _ = img.shape
-img = cv2.resize(img, (img_w//shrink_koeff, img_h//shrink_koeff), cv2.INTER_NEAREST)
+#img: np.ndarray = cv2.imread('robot.jpeg', cv2.IMREAD_COLOR)
+#shrink_koeff = 2
+#img_h,img_w, _ = img.shape
+#img = cv2.resize(img, (img_w//shrink_koeff, img_h//shrink_koeff), cv2.INTER_NEAREST)
 
 cv2.namedWindow("TrackBars")
 cv2.resizeWindow("TrackBars", 640,240)
@@ -19,6 +26,7 @@ cv2.createTrackbar("Val Min", "TrackBars", 0, 255,empty)
 cv2.createTrackbar("Val Max", "TrackBars", 255, 255,empty)
 
 while True:
+    success, img = cap.read()
     imgHSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
     h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
@@ -35,4 +43,3 @@ while True:
     cv2.imshow("img", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-        nigers is black
