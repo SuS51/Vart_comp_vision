9d4402f2d8e27672bd0283f81b2b8a287a23cbd7
SuS <vartevanyan.k@mail.ru>
2021-10-27 16:19:19 +0300

    feat: трекбары для определения цвета на фото
    Для подбора цвета необходимо определить диапазон цветов в системе HSV. Для этого сделали 6 бегунков.

--- Color_detection.py
+++ Color_detection.py
@@ -1,10 +1,38 @@
 import cv2
 import numpy as np
 
+def empty(g):
+    pass
+
 img: np.ndarray = cv2.imread('robot.jpeg', cv2.IMREAD_COLOR)
 shrink_koeff = 2
 img_h,img_w, _ = img.shape
 img = cv2.resize(img, (img_w//shrink_koeff, img_h//shrink_koeff), cv2.INTER_NEAREST)
 
-cv2.imshow("img", img)
-cv2.waitKey()
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640,240)
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179,empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 179, 179,empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 0, 255,empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 255, 255,empty)
+cv2.createTrackbar("Val Min", "TrackBars", 0, 255,empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255,empty)
+
+while True:
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img, img, mask=mask)
+    cv2.imshow("imgResult", imgResult)
+    cv2.imshow("img", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        nigers is black
